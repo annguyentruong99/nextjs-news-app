f82e29ae80c5e464563e695cda6fd9e06e76fedd
An Nguyen <ntan1@cmc.com.vn>
2021-12-20 09:30:40 +0700

    added lighthouserc config file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar';
 
-import BreakingNews from 'components/BreakingNews';
 import axios from 'axios';
 
 const Home: React.FC = () => {
