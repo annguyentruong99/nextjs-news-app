898c3e51ab4d14f82cc31fa326ff3f3dd9d59f17
An Nguyen <ntan1@cmc.com.vn>
2021-12-28 10:11:57 +0700

    removed number of runs config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,7 @@
-// const fs = require("fs");
-
-// const urlsArr = fs.readFileSync("./lh-urls.csv").toString().split("\n");
-
 module.exports = {
 	ci: {
 		collect: {
 			url: ["https://www.executivecentre.com/"],
-			numberOfRuns: 1,
 		},
 		upload: {
 			target: "temporary-public-storage",
